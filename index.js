c5573c1b6be11a26337ff65d2ff2f24875c0ff0f
Dachtrakun <dachtrakun.srima@gmail.com>
2020-11-21 01:12:33 +0700

    top-test

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ let outputNum = document.getElementById('output-num');
 
 
 // ฟังก์ชั่นหาสูตรคูรแบบ กดปุ่ม Action
-function showMultiply(){
+function showMultiply() {
     let number = Number(inputNum.value);
     let outputHtml = '';
 
-    if (number == 0){
+    if (number == 0) {
         outputNum.innerHTML = '<h3 class="text-center mt-5">0 คูณกับอะไรก็ได้ 0</h3>';
         return;
     }
-    for(let i=1; i <= 12; i++){
+    for (let i = 1; i <= 12; i++) {
         outputHtml += '<p>';
         outputHtml += number + ' x ' + i + ' = ' + (number * i);
         outputHtml += '</p>';
@@ -25,53 +25,50 @@ function showMultiply(){
 // calbutton.addEventListener('click',showMultiply);
 
 // ฟังก์ชั่นหาสูตรคูณ แบบใส่ตัวเลขแล้วคำนวณเลย
-function showMultiplyKeyUp(){
+function showMultiplyKeyUp() {
     let number = Number(inputNum.value);
     let outputHtml = ""
 
-    if (number == 0){
+    if (number == 0) {
         outputNum.innerHTML = '<h3 class="text-center mt-5">0 คูณกับอะไรก็ได้ 0</h3>';
         return;
     }
-    for(let i=1; i <= 12; i++){
+    for (let i = 1; i <= 12; i++) {
         outputHtml += '<p>';
         outputHtml += number + ' x ' + i + ' = ' + (number * i);
         outputHtml += '</p>';
     }
     outputNum.innerHTML = outputHtml;
 }
-inputNum.addEventListener('keyup',showMultiplyKeyUp);
+inputNum.addEventListener('keyup', showMultiplyKeyUp);
 
 
 // console.log(price[0].innerHTML);
 // console.log(quantity[1].value);
 // let parsePrice = parseInt(price[0].innerHTML);
 var price = document.getElementsByClassName('price-per-unit');
-    var quantity = document.getElementsByClassName('quantity');
-    var sumprice = document.getElementsByClassName('sum');
-    var totalPrice = document.getElementById('total');
-    var totaltotal = 0;
+var quantity = document.getElementsByClassName('quantity');
+var sumprice = document.getElementsByClassName('sum');
+var totalPrice = document.getElementById('total');
+var totaltotal = 0;
 
-function sumTotal(){
+function sumTotal() {
+    for (var i = 0; i < price.length; i++) {
 
-    
-
-    for(var i=0; i<price.length; i++){
-        
         var parsePrice = parseInt(price[i].innerHTML);
         var parseQuantity = parseInt(quantity[i].value);
-        var totalsum = parsePrice * parseQuantity ;
+        var totalsum = parsePrice * parseQuantity;
         console.log(parseQuantity);
         totaltotal += totalsum
-        sumprice[i].value = totalsum ;
-        
+        sumprice[i].value = totalsum;
+
     }
-
     totalPrice.innerHTML = totaltotal;
-    
 }
-for(var i=0; i<quantity.length; i++){
 
-    quantity[i].addEventListener('keyup',sumTotal);
+
+for (var i = 0; i < quantity.length; i++) {
+
+    quantity[i].addEventListener('keyup', sumTotal);
 }
 
